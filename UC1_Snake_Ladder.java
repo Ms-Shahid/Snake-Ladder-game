50e49d2b740a5c6743f3d260fc1c38da48a65851
Ms-Shahid <ms2406shahid@gmail.com>
2021-08-24 01:21:40 +0530

    random player is playing game using random dice

--- UC1_Snake_Ladder.java
+++ UC1_Snake_Ladder.java
@@ -1,11 +1,29 @@
 package day_4_pratice_problems.snakeLadder;
 
 public class UC1_Snake_Ladder {
-	public static void main(String[] args) {	
-		System.out.println("The Player rolls the die\r\n"
-				+ "to get a number\r\n"
-				+ "between 1 to 6.");
+
+	public static void main(String[] args) {
+		System.out.println("Snake And ladder Game using Single Player");
+		System.out.println("Game Starts Here !! (: ");
+		int counter = 0, position = 0;
+		
 		int Rolling = (int) Math.floor(Math.random() * 10 ) % 6 + 1;
-		System.out.println(Rolling);	
-	}
+		int randomPlayer = (int)Math.floor(Math.random() * 10 ) % 3;
+		
+		switch(randomPlayer) {
+		case 0: System.out.println("No Play ");position = 0;break;
+		case 1: System.out.println("Moving Ahead");position = (int) (position + Rolling);break;
+		default : System.out.println("Moving back");Rolling = (int) (Rolling - position);
+			position = (int) (position - Rolling);
+			if (Rolling > position ) {
+				position = (int) Rolling-position;
+			}
+			else if (position < 0) {
+				position = 0;
+			}break;
+		}
+		counter = position+counter;
+		System.out.println("Current Count of Player is " + counter);
+		System.out.println("----------------------------");
+		}
 }
