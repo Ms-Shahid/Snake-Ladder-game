ede2958c3e515d5ae8e55df306479dfaaacbe1ad
Ms-Shahid <ms2406shahid@gmail.com>
2021-08-24 01:34:31 +0530

    random play in a loop until, player wins

--- UC1_Snake_Ladder.java
+++ UC1_Snake_Ladder.java
@@ -3,37 +3,35 @@ package day_4_pratice_problems.snakeLadder;
 public class UC1_Snake_Ladder {
 
 	public static void main(String[] args) {
-		
 		System.out.println("Snake And ladder Game using Single Player");
 		System.out.println("Game Starts Here !! (: ");
-		
 		int counter = 0, position = 0;
-		while (counter <= 100) {
-		int Rolling = (int)Math.floor(Math.random() * 10 ) % 6 + 1;
-		int randomPlayer = (int)Math.floor(Math.random() * 10 ) % 3;
+		while (counter <= 100 || counter > 100) {
+			if (counter > 100) {
+				counter = counter - position;
+			}
+			else if (counter == 100) {
+					System.out.println("** Wooo! Player Wins! **");
+					break;
+				}
+			int Rolling = (int)Math.floor(Math.random() * 10 ) % 6 + 1;
+			int randomPlayer = (int)Math.floor(Math.random() * 10 ) % 3;
 
-		switch(randomPlayer) {
-		case 0: System.out.println("No Play ");position = 0;break;
-		case 1: System.out.println("Moving Ahead");position = (int) (position + Rolling);break;
-		default : System.out.println("Moving back");Rolling = (int) (Rolling - position);
-			position = (int) (position - Rolling);
-			if (Rolling > position ) {
-				position = (int) Rolling-position;
+			switch(randomPlayer) {
+			case 0: System.out.println("No Play ");position = 0;break;
+			case 1: System.out.println("Moving Ahead");position = (int) (position + Rolling);break;
+			default : System.out.println("Moving back");Rolling = (int) (Rolling - position);
+				position = (int) (position - Rolling);
+				if (Rolling > position ) {
+					position = (int) Rolling-position;
+				}
+				else if (position < 0) {
+					position = 0;
+				}break;
 			}
-			else if (position < 0) {
-				position = 0;
-			}break;
+			System.out.println("Current Count of Player is " + counter);
+			counter = position+counter;
+			System.out.println("----------------------------");
 		}
-		System.out.println("Current Count of Player is " + counter);
-		counter = position+counter;
-		System.out.println("----------------------------");
-		if (counter == 100) {
-			System.out.println("Player Wins!!!");
-			break;
-			}
-		}
-		
-		
 	}
-
 }
