e4fff3c23f5d7108cb0cdba89b4b15d6be3c0528
Ms-Shahid <ms2406shahid@gmail.com>
2021-08-24 01:08:43 +0530

    initial display message

---
+++ UC1_Snake_Ladder.java
@@ -0,0 +1,11 @@
+package day_4_pratice_problems.snakeLadder;
+
+public class UC1_Snake_Ladder {
+	
+	public static void main(String[] args) {	
+		System.out.println("Snake and Ladder\r\n"
+				+ "game played with\r\n"
+				+ "single player at start\r\n"
+				+ "position 0");
+	}
+}
